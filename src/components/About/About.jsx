3642dfb56976486bf98ddb4b250ffc0e39d8cea1
Anshika Shekhar <anshikashekhar13@gmail.com>
2024-07-16 20:35:10 +0530

    react

---
+++ src/components/About/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./About.module.css";
+import aboutImage from "../../assets/about/aboutImage.png";
+import cursorIcon from "../../assets/about/cursorIcon.png";
+import serverIcon from "../../assets/about/serverIcon.png";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={aboutImage}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={cursorIcon} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with interest in building responsive
+                and optimized sites.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={serverIcon} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have interest in developing fast and optimized back-end systems using my skillset which include Node.js, Java.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
